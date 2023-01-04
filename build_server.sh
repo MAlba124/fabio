b42b5dfc399e4fa34adc0ce5176bc4293b7807a4
MAlba124 <0x583po@protonmail.com>
2023-01-04 18:04:56 +0100

    Updated build_server script

--- build_server.sh
+++ build_server.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
 
-cd server/include
-wget https://raw.githubusercontent.com/marzer/tomlplusplus/master/toml.hpp
+TOML_HPP_PATH="./server/include/toml.hpp"
+
+if [ -e  $TOML_HPP_PATH ]
+then
+	printf "[✓ ] Toml++ found\n"
+else
+	printf "[✗ ] Toml++ was not found, downloading...\n"
+	wget https://raw.githubusercontent.com/marzer/tomlplusplus/master/toml.hpp -O $TOML_HPP_PATH
+fi
+
+printf "Running CMake...\n"
+
+if [ -e "server/build" ]
+then
+	cd server/build
+else
+	mkdir server/build && cd server/build
+fi
+
+cmake ..
+
+printf "Compiling...\n"
+
+make -j4
