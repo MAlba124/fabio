82ee9f379ada41e1eed9ca7408fd7829538bdee1
MAlba124 <0x583po@protonmail.com>
2023-01-19 20:15:26 +0100

    Fixed logger not flushing to output file, log to stdout

--- server/log.cpp
+++ server/log.cpp
@@ -4,26 +4,40 @@
 #include <boost/log/sinks/text_file_backend.hpp>
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/console.hpp>
 
 #include "./include/log.hpp"
 
-// TODO: Make the logger flush the file more often
 void
 servLog::init(const std::string& logFile)
 {
-    logging::register_simple_formatter_factory<logging::trivial::severity_level, char>("Severity");
+    logging::register_simple_formatter_factory
+            <logging::trivial::severity_level, char>("Severity");
 
     logging::add_file_log
     (
+            /* Add the logfile number before logFile */
             logging::keywords::file_name = "%N_" + logFile,
+
+            /* Rotate files every 25 MiB */
             logging::keywords::rotation_size = 25 * 1024 * 1024,
-            logging::keywords::format = "[%TimeStamp%] [%Severity%] :: %Message%"
+
+            /* The log message format */
+            logging::keywords::format =
+                    "[%TimeStamp%] [%Severity%] :: %Message%",
+
+            /* Enable automatic flushing */
+            logging::keywords::auto_flush = true
     );
 
     logging::core::get()->set_filter
     (
             logging::trivial::severity >= logging::trivial::info
     );
+
+    /* Output to stdout too */
+    logging::add_console_log(std::cout, boost::log::keywords::format =
+            "[%TimeStamp%] :: %Message%");
 
     logging::add_common_attributes();
 }
