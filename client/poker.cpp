fb14a26ecca44d4ab40c678ea489d06ffc230891
ILoveEveryone24 <ernestascizas2404@gmail.com>
2023-01-23 12:28:19 +0100

    60 million lines, here I come

--- client/poker.cpp
+++ client/poker.cpp
@@ -21,6 +21,10 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 vector<Model> objects;
 vector<glm::vec3> object_axis;
 
+int width = 800;
+int height = 600;
+
+
 int main()
 {
   glfwInit();
@@ -38,6 +42,7 @@ int main()
   }
   glfwMakeContextCurrent(window);
 
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
   IMGUI_CHECKVERSION();
   ImGui::CreateContext();
   ImGuiIO& io = ImGui::GetIO(); (void)io;
@@ -52,13 +57,16 @@ int main()
     return -1;
   }
 
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
   stbi_set_flip_vertically_on_load(false);
 
   glEnable(GL_DEPTH_TEST);
+  glEnable(GL_STENCIL_TEST);
+  glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
 
   Shader ourShader("include/shader.vs", "include/shader.fs");
+
+  Shader stencilShader("include/stencilSh.vs", "include/stencilSh.fs");
 
   float returnVal = 0.0f;
   float returnVal2 = 0.0f;
@@ -70,7 +78,7 @@ int main()
     ImGui::NewFrame();
 
     glClearColor(0.1f, 0.0f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
     ImGui::Begin("Hello Imgui");
     ImGui::Text("hello, this is ImGui");
@@ -103,6 +111,8 @@ int main()
     }
     ImGui::End();
 
+    glStencilFunc(GL_ALWAYS, 1, 0xFF);
+    glStencilMask(0xFF);
 
     ourShader.use();
 
@@ -114,6 +124,7 @@ int main()
     view = glm::rotate(view, glm::radians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
     ourShader.setMat4("projection", projection);
     ourShader.setMat4("view", view);
+
 
     for(int i = 0; i < objects.size(); i++)
     {
@@ -141,6 +152,28 @@ int main()
 
         objects[i].Draw(ourShader);
     }
+    
+    glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+    glStencilMask(0x00);
+    glDisable(GL_DEPTH_TEST);
+    stencilShader.use();
+    glUniform1f(glGetUniformLocation(stencilShader.ID, "stencil"), 1.08f);
+
+    for(int i = 0; i < objects.size(); i++)
+    {
+        glm::mat4 model = glm::mat4(1.0f);
+
+        model = glm::translate(model, object_axis[i]);
+        model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));
+
+        stencilShader.setMat4("model", model);
+
+        objects[i].Draw(stencilShader);
+    }
+
+    glStencilMask(0xFF);
+    glStencilFunc(GL_ALWAYS, 0, 0xFF);
+    glEnable(GL_DEPTH_TEST);
 
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
